16626bf81352ccc987ba049d6eb1130c23be8a46
Alexander Martinz <eviscerationls@gmail.com>
2013-09-18 22:31:54 +0200

    Fix
    Change-Id: I026b142d9f4e6fdf7644d02772d36c6e7fc3c3e3

--- packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Region.Op;
 import android.opengl.GLUtils;

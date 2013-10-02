a275d81dbe715062e695c98fabaaf489b925eb53
Alexander Martinz <eviscerationls@gmail.com>
2013-10-02 15:47:13 +0200

    Add network usage stats to status bar. [1/2]
    Change-Id: I91357e3b3ebeb2d997acfe6841752ea8b130bf0e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2858,6 +2858,18 @@ public final class Settings {
          */
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
+	/**
+         * Whether to show the network status in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS = "status_bar_network_stats";
+
+        /**
+         * Frequency at which stats are updated, in milliseconds
+         * @hide
+         */
+        public static final String STATUS_BAR_NETWORK_STATS_UPDATE_INTERVAL = "status_bar_network_stats_update_frequency";
+
         /**
          * Whether to collapse the notification area after dismissing the last notification
          * @hide

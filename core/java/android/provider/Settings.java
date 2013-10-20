be618387af4c562e75f35a11fef2e30851585680
Alexander Martinz <eviscerationls@gmail.com>
2013-10-20 13:46:15 +0200

    Active display: Allow apps to be excluded from being displayed [1/2]
    Change-Id: Icdaeee60b819c54132dd166c487e10ae6b3d236a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2823,6 +2823,14 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_SUNLIGHT_MODE = "active_display_sunlight_mode";
 
         /**
+         * A list of packages to exclude from being displayed in active display.
+         * This should be a string of packages separated by |
+         *
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_EXCLUDED_APPS = "active_display_excluded_apps";
+
+        /**
          * Whether music controls should be shown on the lockscreen if a supporting
          * music player is active.
          *

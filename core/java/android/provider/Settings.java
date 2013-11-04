58d3f0d1b52c8d79d55cab9fdc1dc237a54da437
Alexander Martinz <eviscerationls@gmail.com>
2013-11-04 21:31:47 +0100

    Squashed update of QuickSettingsTile
    * Collapse Bar when longpress on Halo QuickTile
    * Add support for flipping tiles on click
    
    Change-Id: I4b8b4c5578bff583f2fe56ceaae38c5ed189da32

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3133,6 +3133,13 @@ public final class Settings {
         public static final String QUICK_SETTINGS_RIBBON_TILES = "quick_settings_ribbon_tiles";
 
         /**
+         * Animate-flip Quick Settings Panel Tiles on click
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_FLIP = "quick_settings_tiles_flip";
+
+        /**
          * Navigation controls to Use
          *
          * @hide
